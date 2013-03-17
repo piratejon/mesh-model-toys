70c619c3740dbc9beb72d2b6fc2934e4efa551e9
Jonathan Wesley Stone <jonathanwesleystone@gmail.com>
2013-03-17 18:23:26 -0500

    guid should return something

--- guid.js
+++ guid.js
@@ -7,7 +7,7 @@ Attributed in accordance with <http://stackoverflow.com/questions/4530182/using-
   ***/
 
 function guid() {
-    'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
         var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
         return v.toString(16);
     });
